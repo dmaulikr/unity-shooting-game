2e2012be0916e79ab40035e0c3ff04654fdf8d66
ckcks12 <wyoducc@gmail.com>
2017-05-31 00:16:34 +0900

    *init

---
+++ Assets/Lantern.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Lantern : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        SceneManager.sceneLoaded += SceneManager_sceneLoaded;
+	}
+
+    private void SceneManager_sceneLoaded(Scene arg0, LoadSceneMode arg1)
+    {
+        Debug.Log(arg0.name + " this scene has been loaded!");
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
